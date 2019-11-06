0af99a5dd991ac5001191fc9c31f69579b9cf92e
Santiago Llapur <santillapur@gmail.com>
2019-11-06 14:26:55 -0300

    developed controller and new/edit views

--- app/controllers/kits_controller.rb
+++ app/controllers/kits_controller.rb
@@ -1,6 +1,8 @@
 class KitsController < ApplicationController
-
+  before_action :set_kit, only: [:show, :edit, :update, :destroy]
+  # skip_before_action :authenticate_user!, only: [:index, :show]
   def index
+    @kits = Kit.all
   end
 
   def show
@@ -12,11 +14,31 @@ class KitsController < ApplicationController
   end
 
   def create
+    @kit = Kit.new(kit_params)
+    # kit.user = current_user
+    # authorize kit
+    return redirect_to @kit if @kit.save
+
+    render :new
   end
 
   def edit
   end
 
   def update
+    return redirect_to @kit if @kit.update(kit_params)
+
+    render :edit
+  end
+
+  private
+
+  def set_kit
+    @kit = Kit.find(params[:id])
+    # authorize @kit
+  end
+
+  def kit_params
+    params.require(:kit).permit(:name, :stock, :price, :size, :preparation_time, :image, :description)
   end
 end
