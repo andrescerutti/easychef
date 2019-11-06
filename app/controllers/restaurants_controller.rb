14d141de45a119191e44cca2c6c395514d007cdb
Alejo Gonzales Chaves <alejoagchaves@gmail.com>
2019-11-06 15:30:46 -0300

    still working

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,30 @@
 class RestaurantsController < ApplicationController
+before_action
+  def index
+    restaurants = Restaurant.all
+  end
+
   def show
+    restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    restaurant = Restaurant.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 end
