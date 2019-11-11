1e79805ca4d14d28b7f5a1308793b32a2378bdb4
Enzo Monteverdi <enzomonteverdi@gmail.com>
2019-11-11 19:04:25 -0300

    linking the category cards from index and home to category show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :components]
 
   def home
+    @categories = Category.all
   end
 
   def components
