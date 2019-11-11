f792be5f89cae915a9acccd5cd3fe515efb09f92
Enzo Monteverdi <enzomonteverdi@gmail.com>
2019-11-11 19:43:03 -0300

    matching routes

--- app/controllers/kit_categories_controller.rb
+++ app/controllers/kit_categories_controller.rb
@@ -1,4 +1,7 @@
 class KitCategoriesController < ApplicationController
   def show
+    @kit_category = KitCategory.find(params[:id])
+    @kits = Kit.joins(:categories).where("categories.name = ?", @kit_category.category.name)
+    raise
   end
 end
