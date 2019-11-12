5bf4e2aad31da3e33fbe22d607364ec13d0b80cb
andres cerutti <cerutti_andres@hotmail.com>
2019-11-12 10:25:23 -0300

    order view terminado

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -55,7 +55,7 @@ class OrdersController < ApplicationController
   end
 
   def orders_params
-    params.require(:order).permit(:amount, :state, :check_out_session_id, :code, addresses_atributes: [:address])
+    params.require(:order).permit(:amount, :state, :check_out_session_id, :date_delivery, :code, addresses_atributes: [:address])
   end
 
   def address_params
