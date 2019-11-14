65f10b13eb8239bb8fb5c3c21c5045c257b12010
Enzo Monteverdi <enzomonteverdi@gmail.com>
2019-11-14 13:22:32 -0300

    date style on flatpicker

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -8,7 +8,7 @@ require("flatpickr/dist/themes/dark.css");
   flatpickr(".datepicker", {
     defaultDate: "today",
     altInput: true,
-    dateFormat: "d.m.Y",
+    altFormat: "F j, Y",
     minDate: "today",
     maxDate: new Date().fp_incr(30),
     enableTime: true
