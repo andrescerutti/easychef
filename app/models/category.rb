5d5d71b25d4e395421fe2ee80a86d72e634dbbfc
Santiago Llapur <santillapur@gmail.com>
2019-12-02 14:02:55 -0300

    fixed categories iteration

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,4 @@ class Category < ApplicationRecord
   has_many :kit_categories, dependent: :destroy
   has_many :kits, through: :kit_categories
   validates :name, presence: true, uniqueness: true
-
 end
