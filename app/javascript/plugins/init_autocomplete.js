08709ef7d68d6d8a0f47b1f03c61b9a22d4a2623
Alejo Gonzales Chaves <alejoagchaves@gmail.com>
2019-12-03 22:11:30 -0300

    now yes working

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -3,8 +3,11 @@ import places from 'places.js';
 const initAutocomplete = () => {
   const addressInput = document.querySelector('#query_address');
   const addressInput2 = document.querySelector('#order_address_address');
-  if (addressInput, addressInput2) {
-    places({ container: (addressInput, addressInput2) });
+  if (addressInput) {
+    places({ container: addressInput });
+  }
+  if (addressInput2) {
+    places({ container: addressInput2 });
   }
 };
 
