fe951d9b7d0e57e28c1262053b2944093cafcb2e
Alejo Gonzales Chaves <alejoagchaves@gmail.com>
2019-12-03 22:01:05 -0300

    algolia and diabled search btn when empty input in home

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,10 +1,12 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.getElementById('query_address');
-  if (addressInput) {
-    places({ container: addressInput });
+  const addressInput = document.querySelector('#query_address');
+  const addressInput2 = document.querySelector('#order_address_address');
+  if (addressInput, addressInput2) {
+    places({ container: (addressInput, addressInput2) });
   }
 };
+
 
 export { initAutocomplete };
